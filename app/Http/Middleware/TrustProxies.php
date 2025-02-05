85f06e09b9fb0c7de83852e37717486d328f4626
joaoandreotti <joao.andreotti@hotmail.com>
2025-02-05 12:24:38 +0000

    more proxies configurations

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -9,22 +9,3 @@ class TrustProxies extends Middleware
     protected $proxies = '*';
     protected $headers = Request:: HEADER_X_FORWARDED_AWS_ELB;
 }
-
-/**
-namespace App\Http\Middleware;
-
-use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-class TrustProxies
-{
-    protected $proxies = '*';
-    protected $headers = Request:: HEADER_X_FORWARDED_AWS_ELB;
-
-    public function handle(Request $request, Closure $next): Response
-    {
-        return $next($request);
-    }
-}
-*/
---
+++ app/Kernel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * These middleware are run during every request to your application.
+     *
+     * @var array
+     */
+    protected $middleware = [
+        \App\Http\Middleware\TrustProxies::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+    ];
+    protected $routeMiddleware = [
+    ];
+}
